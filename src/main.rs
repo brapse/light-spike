931e7668c09652b1cf3cb28448e514caad836c40
Sean Braithwaite <brapse@gmail.com>
2020-05-22 09:51:28 +0200

    sync

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+/// TODO: Motivate architecture with flows of events going through components
 /// A spike on the light client event architecture.
 /// The goal:
 /// 1. Split up IO from logic
@@ -6,16 +6,18 @@
 /// 2. Have the light client generated the trusted state that provide read only access to that
 ///    trusted state in seperate threads
 use std::{thread,time};
-use std::time::{Duration};
-use crossbeam::{channel, tick, select};
+use std::time::{Instant, Duration};
+use crossbeam::channel;
 use light_spike::{TrustedState, TSReadWriter};
 
 // IO
+#[derive(Debug)]
 enum IOEvent {
     NoOp(),
     Request(),
     Response(),
 }
+
 struct IO {
 }
 
@@ -29,7 +31,7 @@ impl IO {
     }
 }
 
-// Verifier
+#[derive(Debug)]
 enum VerifierEvent {
     NoOp(),
     NextRequest(),
@@ -39,6 +41,7 @@ enum VerifierEvent {
 }
 
 struct Verifier {
+    // 
 }
 
 impl Verifier {
@@ -55,25 +58,29 @@ struct LightClient {
     io: IO,
     verifier: Verifier,
     trusted_state: TSReadWriter,
+    last_progress: Instant,
+    timeout: Duration,
 }
 
-// Tick => Verifier(NextRequest) => IO(Request) => Verifier(Response) => 
 impl LightClient {
     fn new(trusted_state: TSReadWriter, io: IO, verifier: Verifier) -> LightClient {
         return LightClient {
             trusted_state: trusted_state,
             io,
             verifier,
+            last_progress: Instant::now(),
+            timeout: Duration::from_millis(1),
         }
     }
 
+    // TODO: Align Events with ADR
     fn handle(&mut self, event: Event) -> Event {
-        println!("event!");
+        println!("event! {:?}", event);
         match event {
-            Event::Tick() => {
-                // XXX: Right now we initial a single flow, directly to verifiers.
-                // In the case in which we have multiple finite state machine flows, including
-                // timeouts, we can initiate different flows in a deterministic order.
+            Event::Tick(instant) => {
+                if instant.saturating_duration_since(self.last_progress) > self.timeout {
+                    return Event::Timeout()
+                }
                 let next = VerifierEvent::NextRequest();
                 return self.verifier.handle(next).into()
             },
@@ -86,30 +93,35 @@ impl LightClient {
                 return self.verifier.handle(next).into()
             },
             Event::VerifierEvent(VerifierEvent::Verified()) => {
+                self.last_progress = Instant::now();
                 let next = VerifierEvent::NextRequest();
                 return self.verifier.handle(next).into()
             },
-            Event::NoOp() => {
-                // Probably route to the verifier
-                return Event::Tick();
-            },
-            _ => return Event::Tick(), // TODO:  Remove this when complete
+            _ => return Event::Tick(Instant::now()),
         }
     }
 
-    fn run(mut self, sender: channel::Sender<Event>, receiver: channel::Receiver<Event>) {
+    fn run(mut self,
+        sender: channel::Sender<Event>,
+        receiver: channel::Receiver<Event>,
+        output: channel::Sender<Event>) {
+        self.last_progress = Instant::now();
         thread::spawn(move || {
-            // XXX: We can have a timeout here for liveliness if we like
             loop {
                 let event = receiver.recv().unwrap();
                 match event {
                     Event::Terminate() => {
                         println!("Terminating node");
+                        output.send(Event::Terminated()).unwrap();
+                        return
+                    },
+                    Event::Timeout() => {
+                        output.send(event).unwrap();
                         return
                     },
                     _ => {
                         let next = self.handle(event);
-                        sender.send(next).unwrap(); // TODO: handle error
+                        sender.send(next).unwrap();
                     },
                 }
             }
@@ -117,11 +129,15 @@ impl LightClient {
     }
 }
 
+
+#[derive(Debug)]
 enum Event {
     IOEvent(IOEvent),
     VerifierEvent(VerifierEvent),
     Terminate(),
-    Tick(),
+    Terminated(),
+    Tick(Instant),
+    Timeout(),
     NoOp(),
 }
 
@@ -145,19 +161,12 @@ fn main() {
     let io = IO::new();
     let light_client = LightClient::new(ts_read_writer, io, verifier);
 
-    let (sender, receiver) = channel::bounded::<Event>(1);
+    let (sender, receiver) = channel::unbounded::<Event>();
+    let (output_sender, output_receiver) = channel::unbounded::<Event>();
     let internal_sender = sender.clone();
-    light_client.run(internal_sender, receiver);
+    light_client.run(internal_sender, receiver, output_sender);
 
-    sender.send(Event::Tick());
-    println!("sleeping");
-    let ten_millis = time::Duration::from_millis(100);
-    thread::sleep(ten_millis);
-    println!("done sleeping");
-    // neeed to synchronize this
-    sender.send(Event::Terminate());
-
-    let ten_millis = time::Duration::from_millis(100);
-    thread::sleep(ten_millis);
-    // TODO: remove sleeps with actual termination
+    sender.send(Event::Tick(Instant::now()));
+    let conclusion = output_receiver.recv().unwrap();
+    println!("Conclusion: {:?}", conclusion);
 }
